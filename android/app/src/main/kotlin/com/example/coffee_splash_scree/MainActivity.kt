23d4c2b9eedc6b8e2d26a64d348b3eece2034000
Amospk2 <googletraduzpramin@hotmail.com>
2022-11-07 20:13:09 -0300

    estruturando as pastas dos projetos

--- android/app/src/main/kotlin/com/example/coffee_splash_scree/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coffee_splash_scree
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ coffe_splash_screen/android/app/src/main/kotlin/com/example/coffee_splash_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coffee_splash_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
